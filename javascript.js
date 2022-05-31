7a71f36b46619243eec12ceecd1eb6cc37ca9898
Andy Bui <at.bui89@gmail.com>
2022-05-31 16:02:38 -0400

    Added button to specify grid size

--- javascript.js
+++ javascript.js
@@ -1,24 +1,38 @@
 // Sets important constants and variables
-
 const container = document.getElementById("gridContainer");
 
-let gridSize = 16;
-let numOfPixels = gridSize * gridSize;
+function startGridProcess () {
+    gridSize = prompt("Please enter the length of your desired grid");
+    if (isNaN(gridSize)) {
+        alert('That is NOT a number. Please enter a number');
+    } else if (gridSize > 100) {
+        alert('Please enter a number less than 100');
+    } else { 
+        buildGrid();
+    }
+}
 
-function createDivs (gridSize) {
+function buildGrid() {
+    removeCells();
+    let numOfPixels = gridSize * gridSize;
     for (let i = 0; i < numOfPixels; i++) {
         let row = document.createElement("div");
         row.setAttribute('id', 'cells');
         row.addEventListener('mouseover', function (event) {
             event.target.style.backgroundColor = "purple";
-            console.log("In");
           });
-
         container.appendChild(row);
     }
+    container.setAttribute('style', 'grid-template-columns: repeat('+ gridSize +', 1fr)');
 }
 
-createDivs();
+function removeCells() {
+    while(gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild);
+    }
+  }
+
+
 
 
 
